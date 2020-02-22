b9b23b1672029832bfb07af200b7caa884398eb7
michaldobiezynski <dobiezynski@gmail.com>
2020-02-22 13:07:06 +0000

    understanding lifecycle methods

--- src/index.js
+++ src/index.js
@@ -2,33 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spiner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  //   constructor(props) {
+  //     super(props);
 
-    this.state = { lat: null, errorMessage: '' };
+  //     this.state = { lat: null, errorMessage: '' };
+  //   }
 
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      },
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message }),
     );
+    console.log('My component was rendered to the screen');
   }
 
-  render() {
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!');
+  }
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spiner message='Please accept location request.' />;
+  }
+
+  render() {
+    return <div className='border red'>{this.renderContent()}</div>;
   }
 }
 
